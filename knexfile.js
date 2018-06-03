92a936d3d40485f25eb99b13a4b340e66d195667
S Romberg <SRomberg@Macbook-Pro.local>
2018-06-02 18:58:53 -0600

    ugh

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: 'postgres://localhost:/insta-quotes'
+    connection: 'postgres:///insta-quotes'
   },
   production: {
     client: 'pg',
