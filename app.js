699de90744f39322c797da94fbc4f7bd70858e3a
S Romberg <SRomberg@Macbook-Pro.local>
2018-06-04 09:43:52 -0600

    cors

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const app = express();
 
 const quotes = require("./routes/insta-quotes");
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
